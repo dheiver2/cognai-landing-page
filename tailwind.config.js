107b15d6abe846e7fbb4524c8dd496226d6c4d9c
Dheiver Francisco Santos <165256830+dheiver2@users.noreply.github.com>
2025-01-04 12:50:48 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fadeIn': 'fadeIn 0.2s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          'from': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          'to': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      }
+    },
   },
   plugins: [],
 }
